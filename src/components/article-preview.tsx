0844893d8cbd570ec412d492af46ad8eefe971af
Jason Russell <jasrusable@gmail.com>
2020-06-27 13:11:46 +0200

    Remove unused style

--- src/components/article-preview.tsx
+++ src/components/article-preview.tsx
@@ -4,7 +4,7 @@ import Img from 'gatsby-image';
 import styles from './article-preview.module.css';
 
 export default ({ article }: { article: any }) => (
-  <div className={styles.preview}>
+  <div>
     <Img alt="" fluid={article.heroImage.fluid} />
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
